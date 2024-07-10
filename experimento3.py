b9e65cad41d06e8078b858acd69c9aaa03e25828
Gelsinho <gelsonhenrique122007@gmail.com>
2024-07-10 16:55:38 -0300

    ffgg

---
+++ experimento3.py
@@ -0,0 +1,11 @@
+def hello(nome, idade): 
+    print(f'Olá {nome}!')
+    if int (idade) > 15:
+       print('você pode votar!')
+    else:
+         print('você não pode votar')
+              
+hello('joca', 17)
+
+hello('maisa', 10)
+
